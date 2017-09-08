2c32a2f0529c0577693ebf20452cc2bd6375574f
Sean <toranos@gmail.com>
2017-09-08 21:37:30 +1000

    Add basic header

---
+++ Graph.h
@@ -0,0 +1,37 @@
+#ifndef GDWG_H
+#define GDWG_H
+
+namespace gdwg {
+    template <typename N, typename E> class Graph {
+        public:
+            Graph();
+
+            bool addNode(const N& val);
+            bool addEdge(const N& src, const N& dst, const E& w);
+            bool replace(const N& oldData, const N& newData);
+            void mergeReplace(const N& oldData, const N& newData);
+            void deleteNode(const N&) noexcept;
+            void deleteEdge(const N& src, const N& dst, const E& w) noexcept;
+            void clear() noexcept;
+            bool isNode(const N& val) const;
+            bool isConnected(const N& src, const N& dst) const;
+            void printNodes() const;
+            void printEdges(const N& val) const;
+
+            // the following four operations provide a fake iterator for
+            // enumerating all the node values in a graph. You can abstract a
+            // graph in any sequence, as long as the sequence consists of all
+            // and only the nodes in the graph. 
+            // should be short. use mutable. reference solution uses 1 line per function
+            void begin() const;
+            bool end() const;
+            void next() const;
+            const N& value() const;
+        // private:
+            // mutable iterator?
+    }
+
+    #include "Graph.tem"
+}
+
+#endif
