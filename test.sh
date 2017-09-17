67bb404261ac3c056b3315b4cd72525ca4863e73
Sean <toranos@gmail.com>
2017-09-17 11:54:46 +1000

    Better test script

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
-for number in 1 2 3 4 ; do \
-    g++ -std=c++14 -Wall -Werror -O2 -o test$$number test$$number.cpp; \
-    ./test$$number > result$$number.txt; \
+set -e
+for number in 1 2 3 4 5 6 7 8c 8m 9c 9m 10 11 12 13
+do
+    echo "test $number"
+    g++ -std=c++14 -Wall -Werror -O2 -o test$number test$number.cpp
+    ./test$number > result$number.txt
 done
